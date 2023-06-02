84c6be579800c503ebdf1d9315c6ba1ed38b39c9
rakeshjoshi <rrjoshi2101998@gmail.com>
2023-06-02 12:29:18 +0530

    included 1 in file

--- src/Chatbox.js
+++ src/Chatbox.js
@@ -101,7 +101,7 @@ function Chatbox() {
             onChange={(event) => {
               setInput(event.target.value);
             }}
-            placeholder="Type your message Here"
+            placeholder="Type your message Here 1"
           />
           <button onClick={sendMessage} type="submit">
             {" "}
